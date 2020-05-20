8730095e367cdf7f92c44e25540795179eb30991
HLQ <lequanghung98@gmail.com>
2020-05-21 00:31:05 +0700

    add permissions

--- App/screens/ShowContent.js
+++ App/screens/ShowContent.js
@@ -1,11 +1,27 @@
-import React from 'react'; 
-import {View, Text} from 'react-native'; 
+import React, {useState, useEffect} from 'react'; 
+import {View, FlatList} from 'react-native'; 
+import {connect} from 'react-redux'; 
+import * as actions from '../redux/actions'; 
+import RnderTrack from '../components/RenderTrack'; 
+import {flatListItemLayout} from '../helpers/FlatListLayout';
 
+function ShowContent(props) {
+    const [modal, setModal] = useState({visible: false, item: {}})
 
-export default function Player() {
+    useEffect(() => {
+        const unsubscribe = props.navigation.addListener('focus', props.hideFooter);
+        return unsubscribe
+    }, [props.navigation])
     return (
-        <View>
-            <Text>Show content</Text>
+        <View style = {{flex: 1}}>
+            <FlatList
+                keyExtractor = {(asset) => asset.id.toString() }
+                renderItem = {({item}) => <RenderTrack item={item} setOptions={setModal} /> }
+                data={props.route.params.content}
+                getItemLayout={flatListItemLayout}
+             />
         </View>
     )
 }
+
+export default connect(null, actions)(ShowContent)
--- App/screens/Tracks.js
+++ App/screens/Tracks.js
@@ -1,12 +1,39 @@
-import React from 'react'; 
-import {View, Text} from 'react-native'; 
+import React, {useState, useEffect} from 'react'; 
+import {View, Animated, Text, Dimensions, StatusBar} from 'react-native'; 
+import styled from 'styled-components/native'; 
+import TrackPlayer from 'react-native-track-player'; 
+import {connect} from 'react-redux'; 
+import * as actions from '../redux/actions';
+import RenderTrack from '../components/RenderTrack'; 
+import {flatListItemLayout} from '../helpers/FlatListLayout';
+import {contrastColor} from '../themes/styles'; 
+import setupPlayer from '../services/setupPlayer';
+const ScreenHeight = Dimensions.get('window').height; 
+const StatusBarHeight = StatusBar.currentHeight; 
+const FooterHeight = 60; 
+const BottomTabHeight = 49; 
+const ViewportHeight = ScreenHeight - (StatusBarHeight + FooterHeight + BottomTabHeight)
+const itemHeight = 75; 
+ 
+function Tracks(props) {
+    const [scrollY, setScrollY] = useState(new Animated.Value(0))
+    const [modal, setModal] = useState({visible: false, item: {}})
+    const {currentTrack, mediaLoaded, media}  = props; 
 
-function Track(props) {
-    return (
-        <View>
-            <Text>Track screen</Text>
-        </View>
-    )
+    useEffect(() => {
+        props.getMedia(); 
+        console.log(media)
+    })
+    console.log(scrollY)
+    return (<View><Text>tracks</Text></View>)
 }
 
-export default Track;
+function mapStateToProps(state) {
+    return {
+        currentTrack: state.playback.currentTrack, 
+        media: state.media.mediaFiles, 
+        mediaLoaded: state.media.mediaLoaded
+    }
+}
+
+export default connect(mapStateToProps, actions)(Tracks)
